f4ee72cf1695af764739429d46ab3ee92579af8e
MaksunKar <130152425+MaksunKar@users.noreply.github.com>
2024-07-05 21:43:10 +0300

    Create MakarovkaUniversity.java

---
+++ MakarovkaUniversity.java
@@ -0,0 +1,11 @@
+class MakarovkaUniversity extends University {
+    private Makarovka makarovka;
+
+    public Makarovka getMakarovka() {
+        return makarovka;
+    }
+
+    public void setMakarovka(Makarovka makarovka) {
+        this.makarovka = makarovka;
+    }
+}
